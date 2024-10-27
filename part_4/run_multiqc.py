45a947406c79ffe61b6e7b1f64881aaf8c5b0271
Phil Ewels <phil.ewels@seqera.io>
2024-10-27 23:32:32 +0100

    Part 4: Refactor for readability

--- part_4/run_multiqc.py
+++ part_4/run_multiqc.py
@@ -4,17 +4,15 @@ import multiqc
 multiqc.parse_logs('./fastp')
 
 # Fetch the custom data
-fastp_data = multiqc.get_module_data(module="fastp")
-reads_before_filtering = {
-    s: {
-        'Reads Before Filtering': d['summary']['before_filtering']['total_reads']
+reads = {}
+for samp, data in multiqc.get_module_data(module='fastp').items():
+    reads[samp] = {
+      'Reads Before Filtering': data['summary']['before_filtering']['total_reads']
     }
-    for s, d in fastp_data.items()
-}
 
 # Add new column to the General Stats table
 fastp_module = multiqc.report.modules[0]
-fastp_module.general_stats_addcols(data_by_sample=reads_before_filtering)
+fastp_module.general_stats_addcols(data_by_sample=reads)
 
 # Write the report
 multiqc.write_report(force=True)
